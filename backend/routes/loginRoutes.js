e5141bcfff08429229cbb08b3603192c5ac49965
thusharpradeep <thushar01.pradeep@gmail.com>
2025-12-08 21:11:09 +0530

    loginroutes.js added

---
+++ backend/routes/loginRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const User = require('../models/User');
+
+router.post('/', async (req, res) => {
+  const {username, password} = req.body;
+
+  const user = await User.findOne({username});
+  if (!user) return res.json({status: 'fail', message: 'user_not_found'});
+
+  if (password !== user.password) {
+    return res.json({status: 'fail', message: 'incorrect_password'});
+  }
+
+  res.json({status: 'success', user_id: user._id, role: user.role});
+});
+
+module.exports = router;
