f26c6f5148f2fa4919e959addf12b642ceb1779a
Vishnu pc <vishnupc974@gmail.com>
2025-12-08 20:35:02 +0530

    db.js and connection established with mongoDB'

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+require("dotenv").config();
+
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// Connect database
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("MedSync Backend Running");
+});
+
+// Routes will be added later
+// app.use("/api/doctors", require("./routes/doctorRoutes"));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
