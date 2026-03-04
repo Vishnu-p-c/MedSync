cbf799f5c3b720fc0232d2e1c7a9da08f6efb88e
thusharpradeep <thushar01.pradeep@gmail.com>
2026-03-04 21:56:53 +0530

    EQP STATUS CHANGE

--- backend/routes/equipmentRoutes.js
+++ backend/routes/equipmentRoutes.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
-const {addEquipment} = require('../controllers/equipmentController');
+const {addEquipment, updateEquipmentStatus} =
+    require('../controllers/equipmentController');
 
 // POST /equipment/add - Add new equipment
 router.post('/add', addEquipment);
+
+// POST /equipment/update-status - Update equipment status
+router.post('/update-status', updateEquipmentStatus);
 
 module.exports = router;
